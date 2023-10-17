bd3074264705a260bfccf2ea7392c4773ed34b7c
Katerina Hronikova <katacek@gmail.com>
2023-10-17 12:09:17 +0200

    port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // to enable calling backend locally
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
